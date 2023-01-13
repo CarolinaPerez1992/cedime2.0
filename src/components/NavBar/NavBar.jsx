26bf0641d93108fc63a42a31d80bd94e0acbd690
CarolinaPerez1992 <carolinabperez@outlook.com>
2023-01-12 23:38:40 -0300

    Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,68 +1,67 @@
-import React, { useState } from 'react'
-import Logo from '../../Assets/logo-3cx.png'
-import "./NavBar.css"
-import Bars from '../../Assets/bars.png'
-import { Link } from 'react-scroll'
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from '../../Assets/logo-3cx.png';
+// import navIcon1 from '../assets/img/nav-icon1.svg';
+// import navIcon2 from '../assets/img/nav-icon2.svg';
+// import navIcon3 from '../assets/img/nav-icon3.svg';
+import { HashLink } from 'react-router-hash-link';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 
+function NavBar() {
 
-const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
 
-    const mobile = window.innerWidth <= 768 ? true : false;
-    const [menuOpened, setMenuOpened] = useState(false);
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
 
-    return (
-        <div className='navbar'><img src={Logo} className="logo" alt="logo_cedime" />
-            {menuOpened === false && mobile === true ? (
-                <div style={{ backgroundColor: 'violet', padding: '1rem', borderRadius: '5px' }}
+    window.addEventListener("scroll", onScroll);
 
-                    onClick={() => setMenuOpened(true)}
-                >
-                    <img src={Bars} alt='menu_bars' style={{ width: '1.5rem', height: '1.5rem' }} />
-                </div>
-            ) : (
-                <ul className='navbar-menu'>
-                    <li>
-                        <Link onClick={() => setMenuOpened(false)}
-                            activeClass='active'
-                            to='inicio'
-                            span={true}
-                            smooth={true}>Inicio</Link></li>
-                    <li ><Link onClick={() => setMenuOpened(false)}
-                        to='nosotros'
-                        span={true}
-                        smooth={true}>Nosotros</Link></li>
-                    <li ><Link onClick={() => setMenuOpened(false)}
-                        to='filosofia'
-                        span={true}
-                        smooth={true}>Filosofía</Link></li>
-                    <li ><Link onClick={() => setMenuOpened(false)}
-                        to='servicios'
-                        span={true}
-                        smooth={true}>Servicios</Link></li>
-                    <li ><Link onClick={() => setMenuOpened(false)}
-                        to='galeria'
-                        span={true}
-                        smooth={true}>Galería</Link></li>
-                    <li ><Link onClick={() => setMenuOpened(false)}
-                        to='equipo'
-                        span={true}
-                        smooth={true}>Equipo Profesional</Link></li>
-                    <li ><Link onClick={() => setMenuOpened(false)}
-                        to='blog'
-                        span={true}
-                        smooth={true}>Blog</Link></li>
-                    <li ><Link onClick={() => setMenuOpened(false)}
-                        to='colabo'
-                        span={true}
-                        smooth={true}>Colaboradores</Link></li>
-                    <li ><Link onClick={() => setMenuOpened(false)}
-                        to='contacto'
-                        span={true}
-                        smooth={true}>Contacto</Link></li>
-                </ul>
-            )}
-        </div>
-    )
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/" >
+            <img src={logo} alt="Logo" className="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Inicio</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Nosotros</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Filosofia</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                {/* <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a> */}
+              </div>
+              {/* <HashLink to='#connect'>
+                <button className="vvd"><span>Let’s Connect</span></button>
+              </HashLink> */}
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  )
 }
-
 export default NavBar
