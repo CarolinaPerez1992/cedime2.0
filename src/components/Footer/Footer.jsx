487f6e8aa8b6c8fd0a0836fda0ea99cd55859156
CarolinaPerez1992 <carolinabperez@outlook.com>
2023-01-13 15:40:35 -0300

    backToTop

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Footer.css"
+
+
+const Footer = () => {
+    return (
+        <div className='Footer' id='footer'>
+
+            <div><h1>Footer</h1></div>
+
+        </div>
+    );
+};
+
+export default Footer
