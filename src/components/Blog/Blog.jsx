ce1b40bc6c337a0675a63500221d23398c0beb45
CarolinaPerez1992 <carolinabperez@outlook.com>
2023-01-30 15:24:23 -0300

    update back to top

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import "../Blog/Blog.css"
 
 const Blog = () => {
+  async function fetchApi(){
+    let events = await fetch('http://localhost:8080/posts')
+    events = await events.json()
+    console.log(events)
+  }
+  fetchApi()
   return (
     <div className='Blog' id='blog'>
       <div className='contenedor_titulo'>
       <div className='blob2'></div>
         <h2 className='titulo_filo'>Blog</h2>
       </div>
+
       </div>
   )
 }
